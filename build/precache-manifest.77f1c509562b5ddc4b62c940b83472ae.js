5507507fbaffc67e8eec0c3f4e72310faa993420
RicardoGorki <ricardo.gorki@gmail.com>
2019-11-25 13:37:19 -0300

    update

---
+++ build/precache-manifest.77f1c509562b5ddc4b62c940b83472ae.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb7cbed42a3dcd6047fb58e352d5809",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7317b80efcfbd79b9fe6",
+    "url": "/static/js/2.766cff5f.chunk.js"
+  },
+  {
+    "revision": "dff8cb13d711f4545830",
+    "url": "/static/js/main.d00353cc.chunk.js"
+  },
+  {
+    "revision": "a53a2054f1db8ad2ec81",
+    "url": "/static/js/runtime-main.9da2e07d.js"
+  }
+]);
